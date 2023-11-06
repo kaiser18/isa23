0196da74578d8cb627056d2f44427eb54cf1435f
kaiser18 <mihailoivic@protonmail.com>
2023-11-06 09:43:19 +0100

    backend starter project

---
+++ src/main/java/com/ftn/isa23/Isa23Application.java
@@ -0,0 +1,13 @@
+package com.ftn.isa23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftn/isa23/Isa23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.isa23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isa23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
