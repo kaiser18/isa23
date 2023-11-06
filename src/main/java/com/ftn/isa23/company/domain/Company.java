1f60902a1920874d804cd7e2d3527daea2d60fb2
kaiser18 <mihailoivic@protonmail.com>
2023-11-06 12:44:47 +0100

    fixed dependecies, added company to the model

--- src/main/java/com/ftn/isa23/company/domain/Company.java
+++ src/main/java/com/ftn/isa23/company/domain/Company.java
@@ -1,4 +1,69 @@
 package com.ftn.isa23.company.domain;
 
+import com.ftn.isa23.general.domain.Address;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "companies")
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToOne(cascade = CascadeType.MERGE,fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+    private String description;
+    private double averageGrade;
+
+    public Company() {}
+
+    public Company(Long id, String name, Address address, String description, double averageGrade) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.description = description;
+        this.averageGrade = averageGrade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
 }
