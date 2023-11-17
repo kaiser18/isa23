8db51e9fcc1c68a9cc2cdf819ee596882cf49d9c
kaiser18 <mihailoivic@protonmail.com>
2023-11-17 18:44:14 +0100

    fixed a typo in the sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,9 +7,9 @@ insert into addresses(id, city, country, number, street) values (600, 'Novi Sad'
 insert into addresses(id, city, country, number, street) values (700, 'Novi Sad', 'Serbia', '1', 'Njegoseva');
 
 insert into users(role, id, email, name, password, phone_number, surname, address_id) values ('Customer', 100, 'kaiserbreee@hotmail.com', 'Pera', '$2a$10$xhXx1uOCrlGvXxgxCL88TeYYIw1VGZv0NQ6KCgHF9JhEAB3im3Il.', '12345', 'Peric', 100);
-insert into users(role, id, email, name, password, phone_number, surname, address_id) values ('BloodBankAdmin', 200, 'oliahim.ivic@gmail.com', 'Milan', '$2a$10$xhXx1uOCrlGvXxgxCL88TeYYIw1VGZv0NQ6KCgHF9JhEAB3im3Il.', '12345', 'Milanovic', 200);
-insert into users(role, id, email, name, password, phone_number, surname, address_id) values ('BloodBankAdmin', 300, 'mihailoivic@protonmail.com', 'Milos', '$2a$10$xhXx1uOCrlGvXxgxCL88TeYYIw1VGZv0NQ6KCgHF9JhEAB3im3Il.', '12345', 'Milosevic', 300);
-insert into users(role, id, email, name, password, phone_number, surname, address_id) values ('BloodBankAdmin', 400, 'mihailo.ivic@uns.ac.rs', 'Jovan', '$2a$10$xhXx1uOCrlGvXxgxCL88TeYYIw1VGZv0NQ6KCgHF9JhEAB3im3Il.', '12345', 'Jovanovic', 400);
+insert into users(role, id, email, name, password, phone_number, surname, address_id) values ('CompanyAdmin', 200, 'oliahim.ivic@gmail.com', 'Milan', '$2a$10$xhXx1uOCrlGvXxgxCL88TeYYIw1VGZv0NQ6KCgHF9JhEAB3im3Il.', '12345', 'Milanovic', 200);
+insert into users(role, id, email, name, password, phone_number, surname, address_id) values ('CompanyAdmin', 300, 'mihailoivic@protonmail.com', 'Milos', '$2a$10$xhXx1uOCrlGvXxgxCL88TeYYIw1VGZv0NQ6KCgHF9JhEAB3im3Il.', '12345', 'Milosevic', 300);
+insert into users(role, id, email, name, password, phone_number, surname, address_id) values ('CompanyAdmin', 400, 'mihailo.ivic@uns.ac.rs', 'Jovan', '$2a$10$xhXx1uOCrlGvXxgxCL88TeYYIw1VGZv0NQ6KCgHF9JhEAB3im3Il.', '12345', 'Jovanovic', 400);
 
 insert into companies(id, average_grade, description, name, address_id) values (100, 4.0, 'First company', 'MedEquip', 500);
 insert into companies(id, average_grade, description, name, address_id) values (200, 4.6, 'Second company', 'MedCompany', 600);
